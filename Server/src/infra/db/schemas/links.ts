9ed2fe7bfaf32f4c1cf600bc3bbe4da120cfbf33
WoneyBranga <woney.branga@gmail.com>
2025-05-14 18:57:46 -0300

    finalize db config

--- Server/src/infra/db/schemas/links.ts
+++ Server/src/infra/db/schemas/links.ts
@@ -1,7 +1,7 @@
 import { integer, pgTable, text, timestamp } from 'drizzle-orm/pg-core'
 import { uuidv7 } from 'uuidv7'
 
-export const shortLinks = pgTable('links', {
+export const links = pgTable('links', {
   id: text('id')
     .primaryKey()
     .$defaultFn(() => uuidv7()),
