670c82ef15e418ef2dbfacd456d3c7c1dd92b980
WoneyBranga <woney.branga@gmail.com>
2025-05-14 18:10:13 -0300

    configure swagger

--- Server/src/infra/http/routes/create-url-route.ts
+++ Server/src/infra/http/routes/create-url-route.ts
@@ -1,7 +1,28 @@
+import { or } from 'drizzle-orm'
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
-
+import { z } from 'zod'
 export const createUrlRoute: FastifyPluginAsyncZod = async server => {
-  server.get('/create-url', () => {
-    return 'creating url'
-  })
+  server.post(
+    '/create-url',
+    {
+      schema: {
+        summary: 'Upload an image',
+        body: z.object({
+          originalUrl: z.string().url(),
+          shortUrl: z.string().optional(),
+          accessCount: z.number().optional(),
+          createdAt: z.string().optional(),
+        }),
+        response: {
+          201: z.object({ linkId: z.string() }),
+          409: z
+            .object({ message: z.string() })
+            .describe('Upload already exists.'),
+        },
+      },
+    },
+    async (request, reply) => {
+      return reply.status(201).send({ linkId: '123' })
+    }
+  )
 }
